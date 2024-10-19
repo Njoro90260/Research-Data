4813961c58a7fc87e85ed8e19a178c47934fbe23
njoro90620 <wchegesalome@gmail.com>
2024-10-19 12:19:45 +0300

    formatted output.

--- salting_tq.py
+++ salting_tq.py
@@ -20,8 +20,7 @@ def response():
 r = response()    
 # Calculate mean and standard deviation for each statement
 results = {statement: (np.mean(data), np.std(data)) for statement, data in r.items()}
-print(results)
-
+print(f"Salting Techniques Mean and Standard Deviations\n\n{results}\n\n")
 # Calculate percentage for each unique response per statement
 response_percentages = {}
 
@@ -31,4 +30,4 @@ for statement, data in r.items():
     response_percentages[statement] = percentages
 
 for statement, responses in  response_percentages.items():
-    print(f"{statement}: Strongly Disagree (1): {responses.get(1, 0)}%, Disagree (2): {responses.get(2, 0)}%, Neutral (3): {responses.get(3, 0)}%, Agree (4): {responses.get(4, 0)}%, Strongly Agree (5): {responses.get(5, 0)}%")
+    print(f"Salting Techniques Percetages\n\n{statement}: Strongly Disagree (1): {responses.get(1, 0)}%, Disagree (2): {responses.get(2, 0)}%, Neutral (3): {responses.get(3, 0)}%, Agree (4): {responses.get(4, 0)}%, Strongly Agree (5): {responses.get(5, 0)}%")
--- sys_integ.py
+++ sys_integ.py
@@ -18,7 +18,7 @@ def response_integration():
     return integration_responses
 r = response_integration()
 results = {statement: (np.mean(data), np.std(data)) for statement, data in r.items()}
-print(results)
+print(f"Mean and Standard Deviation: \n\n{results}\n\n")
 
 # Calculate percentage for each unique response per statement
 response_percentages = {}
@@ -30,4 +30,4 @@ for statement, data in r.items():
 
 # Print response percentages
 for statement, responses in response_percentages.items():
-    print(f"{statement}: Strongly Disagree (1): {responses.get(1, 0)}%, Disagree (2): {responses.get(2, 0)}%, Neutral (3): {responses.get(3, 0)}%, Agree (4): {responses.get(4, 0)}%, Strongly Agree (5): {responses.get(5, 0)}%")
+    print(f"System Integration Practices Percentages: \n\n{statement}: Strongly Disagree (1): {responses.get(1, 0)}%, Disagree (2): {responses.get(2, 0)}%, Neutral (3): {responses.get(3, 0)}%, Agree (4): {responses.get(4, 0)}%, Strongly Agree (5): {responses.get(5, 0)}%")
